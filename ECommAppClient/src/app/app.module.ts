fefa9c6fc212275e4325ae12a7d5681f0ef6eef4
pkurremula <pkurremula@gmail.com>
2022-06-07 09:56:54 -0400

    End of Section 7

--- ECommAppClient/src/app/app.module.ts
+++ ECommAppClient/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//import { AppRoutingModule } from '.app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
