42824b57eaaede727ac5e5755568d438c8fc8207
pkurremula <pkurremula@gmail.com>
2022-06-27 10:53:16 -0400

    End of section 8

--- ECommAppClient/src/app/app.component.ts
+++ ECommAppClient/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product'
 
 @Component({
   selector: 'app-root',
@@ -8,35 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[] = [];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5164/api/Products').subscribe((response: any) => {
+    this.http.get('http://localhost:5164/api/Products?pagesize=50').subscribe(
+      (response: any) => {
       console.log(response);
+     this.products = response.data;
     }, error => {
       console.log(error);
     });
   }
-
-  //public forecasts?: WeatherForecast[];
-
-  //constructor(http: HttpClient) {
-  //  http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-  //    this.forecasts = result;
-  //  }, error => console.error(error));
-  //}
-
-  //ngOnInit(): void {
-
-  //}
-
-  //title = 'ECommAppClient';
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
 }
